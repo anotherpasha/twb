d2de2b9ed4c8330568a379d1edb2674c3c8414cf
aldoritma <aldoariandhika@gmail.com>
2016-08-13 01:06:32 +0700

    adding module animated

---
+++ build/js/modules/animated.js
@@ -0,0 +1,43 @@
+(function( global ) {
+
+	 var Animated = function (selector) {
+
+	 	this.selector =  document.querySelectorAll(selector);
+	 	return this;
+	 };
+
+	Animated.prototype.bounce = function(coorX, coorY) {
+		var tween =TweenMax.from(this.selector, 1, {
+			x: coorX,
+			y: coorY,
+			ease: Bounce.easeOut
+		})
+		return tween;
+	};
+
+	Animated.prototype.fading = function(time) {
+		console.log(this.selector)
+		var tween = TweenMax.fromTo(this.selector, time, {
+			opacity: 0
+		},
+		{
+			opacity: 1
+		});
+		return tween;
+
+	};
+
+
+	var animated = function(selector, context) {
+		return new Animated(selector, context);
+
+	}
+
+
+
+	global.animated = animated;
+
+
+})( this );
+
+
