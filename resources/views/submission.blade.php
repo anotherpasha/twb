b4ed181c1ecfb27ae9be34851d0b496349f70067
aldoritma <aldoariandhika@gmail.com>
2016-08-18 14:52:01 +0700

    partialing navbar

--- resources/views/submission.blade.php
+++ resources/views/submission.blade.php
@@ -5,29 +5,7 @@
 
 
 <div class="container__main "><!-- start main container -->
-    <header class="navbar__subpage">
-        <div class="container">
-            
-        <a href="index.html" class="navbar--logo push-half--top"><img src="{{asset('images/logo-small.png')}}"></a>
-
-        <nav class="navigation--home hard">
-            <!-- is login = true -->
-            <ul class="list__inline">
-                <li class=""><a href="javascript:;" class="navigation--link black">Logout</a></li>
-                <li class=""><a href="profile.html" class="navigation--link black">Profilku</a></li>
-                <li class="navigation--button"><a href="submission.html" class="button button__primary button--small " data-target="login">Submit</a></li>
-            </ul>
-
-            <!-- is login = false -->
-
-            <!--ul class="list__inline">
-                <li class=""><a href="javascript:;" class="button button__quertiary  button--small toggle--modal" data-target="login">Login</a></li>
-                <li><a href=""><img class="image--responsive" src="assets/images/logo-twb-small.png"></a></li>
-            </ul-->
-        </nav>
-        </div>
-
-    </header>
+    @include('partials/navbar')
 
     <!-- start submission setion -->
     <section  class="  section  section--subpage text--center">
@@ -61,7 +39,7 @@
                         </div>
                         <div class="form__control">
                             <!--button type="submit" class="button button__secondary button--small disabled">Daftar</button-->
-                            <a href="success-submission.html" class="button button__secondary button--small disabled">Daftar</a>
+                            <a href="/success-submission" class="button button__secondary button--small disabled">Daftar</a>
 
                         </div>
                     </div>
--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -5,29 +5,7 @@
 
 
 <div class="container__main "><!-- start main container -->
-    <header class="navbar__subpage">
-        <div class="container">
-            
-        <a href="/" class="navbar--logo push-half--top"><img src="{{asset('images/logo-small.png')}}"></a>
-
-        <nav class="navigation--home hard">
-            <!-- is login = true -->
-            <ul class="list__inline">
-                <li class=""><a href="javascript:;" class="navigation--link black">Logout</a></li>
-                <li class=""><a href="profile.html" class="navigation--link black">Profilku</a></li>
-                <li class="navigation--button"><a href="submission.html" class="button button__primary button--small " data-target="login">Submit</a></li>
-            </ul>
-
-            <!-- is login = false -->
-
-            <!--ul class="list__inline">
-                <li class=""><a href="javascript:;" class="button button__quertiary  button--small toggle--modal" data-target="login">Login</a></li>
-                <li><a href=""><img class="image--responsive" src="assets/images/logo-twb-small.png"></a></li>
-            </ul-->
-        </nav>
-        </div>
-
-    </header>
+    @include('partials/navbar')
 	<!-- start success setion -->
 	<section  class="  section  section--subpage">
 		
