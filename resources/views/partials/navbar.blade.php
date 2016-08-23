d45abaef94df993f56049a54e138016226149bbf
aldoritma <aldoariandhika@gmail.com>
2016-08-23 16:46:02 +0700

    remove logo twb and fixed src on script

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -8,12 +8,12 @@
             <ul class="list__inline">
                 <li class=""><a href="{!! url('logout') !!}" class="navigation--link black">Logout</a></li>
                 <li class=""><a href="{!! url('profile') !!}" class="navigation--link black">Profilku</a></li>
-                <li class="navigation--button"><a href="{!! url('submission') !!}" class="button button__primary button--small " data-target="login">Submit</a></li>
-            </ul>
+<!--                 <li class="navigation--button"><a href="{!! url('submission') !!}" class="button button__primary button--small " data-target="login">Submit</a></li>
+ -->            </ul>
             @else
             <ul class="list__inline">
                 <li class=""><a href="javascript:;" id="login-button" class="button button__quertiary  button--small toggle--modal" data-target="login">Login</a></li>
-                <li><img class="image--responsive" src="{!! asset('/images/logo-twb-small.png') !!}"></li>
+                <!-- <li><img class="image--responsive" src="{!! asset('/images/logo-twb-small.png') !!}"></li> -->
             </ul>
             @endif
         </nav>
